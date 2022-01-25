cca95f0034564cb1166d541923ce0f4073bd8e78
Raul Macias <maciasr@users.noreply.github.com>
2022-01-25 17:04:09 +0000

    reason for commit

---
+++ advlist/complex02.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - making selections from complex lists"""
+
+def main():
+
+    # create a list called list1
+    list1 = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", ["Cow Hen Koi Hog Jay Kit"]]
+
+    print (list1)
+
+    print (list1[4])
+
+main()
